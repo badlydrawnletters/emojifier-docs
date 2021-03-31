15835ebbafe028fcd0709474489476bd3f177ba7
Techy <52178694+Techy@users.noreply.github.com>
2021-03-30 20:38:39 -0700

    Create changelog.js

---
+++ all/changelog.js
@@ -0,0 +1,13 @@
+# Changelog
+---
+### Description
+This command is used to information about the bot's recent update and any announcements from the developers
+### Usage
+```
+!!changelog 
+```
+### Permission Required
+Anyone can use this command, unless they are blacklisted globally
+
+<!-- ### Example image
+![changelog example](../images/changelog.png) -->
